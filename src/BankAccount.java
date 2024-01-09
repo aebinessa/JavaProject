57c4c4da3c8fed6e2eb82c4acca19c7773ebf0fe
Fawaz J M M A Alzamel <83331@kfh.com>
2024-01-09 15:34:33 +0300

    Add Bank Account Class

---
+++ src/BankAccount.java
@@ -0,0 +1,54 @@
+public class BankAccount {
+    
+int accountNumber;
+
+double balance;
+
+String transactionHistory;
+
+
+public int getAccountNumber() {
+    return accountNumber;
+}
+public void setAccountNumber(int accountNumber) {
+    this.accountNumber = accountNumber;
+}
+
+
+
+public double getBalance() {
+    return balance;
+}
+public void setBalance(double balance) {
+    this.balance = balance;
+}
+
+
+
+
+public String getTransactionHistory() {
+    return transactionHistory;
+}
+public void setTransactionHistory(String transactionHistory) {
+    this.transactionHistory = transactionHistory;
+}
+
+
+
+public void deposit(double despositAmount) {
+    
+    
+
+}
+
+
+public void withdrawal(double withdrawalAmount) {
+    
+}
+
+
+public void  theTransactionHistory(String transactionHistory) {
+    
+    
+}
+}
