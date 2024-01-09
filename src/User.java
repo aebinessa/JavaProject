dc41c31de276c820964d8c026229999410f11e94
aebinessa <abinessa0004@sdsu.edu>
2024-01-09 15:13:56 +0300

    created attributes for user class

--- src/User.java
+++ src/User.java
@@ -1,3 +1,38 @@
 public class User {
+    private String username;
+
+    public User(String username, String password, String account) {
+        this.username = username;
+        this.password = password;
+        this.account = account;
+    }
+
+    private String password;
+
+    private String account;
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
