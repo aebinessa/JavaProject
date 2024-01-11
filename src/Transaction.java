b0bcac6f384a065f7df4d8e1d5975a17846942b6
aebinessa <abinessa0004@sdsu.edu>
2024-01-11 08:31:10 +0300

    transaction

--- src/Transaction.java
+++ src/Transaction.java
@@ -22,7 +22,7 @@ public class Transaction {
         this.amount = amount;
     }
 
-    public Date getdate() {//change getdate to getDate
+    public Date getdate() {
         return date;
     }
 
